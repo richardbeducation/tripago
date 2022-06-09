e8b3b815eeadde6f07a25d374a65b78f9fd5bd4a
richardbeducation <richardbeducation@gmail.com>
2022-06-09 13:29:44 +0700

    update

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,15 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect } from 'react'
+import './TripList.css'
 
 export default function TripList() {
   const [trips, setTrips] = useState([])
+  const [url, setUrl] = useState('http://localhost:3000/trips')
+
+  console.log(trips)
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setTrips(json))
+  }, [url])  
     
-  fetch('http://localhost:3000/trips')
-    .then(response => response.json())
-    .then(json => setTrips(json))
-  
   return (
-    <div>
+    <div className="trip-list">
       <h2>Trip List</h2>
+      <ul>
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+          <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+      <div className='filters'>
+        <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+          European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+          All Trips
+        </button>
+
+      </div>
     </div>
   )
 }
