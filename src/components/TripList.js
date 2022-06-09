967e0ae4ae16441a32d7044ba087635a492d2a23
richardbeducation <richardbeducation@gmail.com>
2022-06-09 08:29:25 +0700

    update

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -2,12 +2,11 @@ import { useState } from "react"
 
 export default function TripList() {
   const [trips, setTrips] = useState([])
-
     
   fetch('http://localhost:3000/trips')
     .then(response => response.json())
-    // .then(json => setTrips(json))
-
+    .then(json => setTrips(json))
+  
   return (
     <div>
       <h2>Trip List</h2>
