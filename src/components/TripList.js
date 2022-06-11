021b39c5d712fb5fc7d599e2e4714ef7aa6369f9
richardbeducation <richardbeducation@gmail.com>
2022-06-11 13:47:31 +0700

    update

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,27 +1,17 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState } from 'react'
 import './TripList.css'
+import { useFetch } from '../hooks/useFetch'
+
 
 export default function TripList() {
-  const [trips, setTrips] = useState([])
   const [url, setUrl] = useState('http://localhost:3000/trips')
+  const { data: trips } = useFetch(url)
 
-  const fetchTrips = useCallback(async () => {
-    const response = await fetch(url)
-    const trips = await response.json()
-    setTrips(trips)
-  }, [url])
-
-  useEffect(() => {
-    console.log('useEffect function ran')
-    fetchTrips()
-  }, [fetchTrips])
-  
-    
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       <ul>
-        {trips.map(trip => (
+        {trips && trips.map(trip => (
           <li key={trip.id}>
             <h3>{trip.title}</h3>
           <p>{trip.price}</p>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,20 @@
 import { useState, useEffect } from 'react'
 
-const useFetch = (url) => {
-  const [data, setData] = useState
+export const useFetch = (url) => {
+  const [data, setData] = useState(null)
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url)
+      const json = await res.json()
+
+      setData(json)
+    }
+
+    fetchData()
+  }, [url])
+
+  return { data: data }
 }
 
 export default useFetch
